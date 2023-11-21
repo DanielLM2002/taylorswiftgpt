5741870ec5f4dc1d33c0e01514f0e908c82fc41f
estebancb03 <esteban.castaneda@ucr.ac.cr>
2023-11-20 18:45:18 -0600

    Configuración incial del front

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': {'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    },
+  },
+  plugins: [],
+}

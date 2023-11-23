2eedda35ea22369a887f2b2abf0c9e0656cd1249
estebancb03 <esteban.castaneda@ucr.ac.cr>
2023-11-23 01:40:25 -0600

    Correccion de imports en Signup

--- src/Frontend/src/pages/Signup.jsx
+++ src/Frontend/src/pages/Signup.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { FaGuitar } from 'react-icons/fa';
 import { IoLogoGoogle } from 'react-icons/io5';

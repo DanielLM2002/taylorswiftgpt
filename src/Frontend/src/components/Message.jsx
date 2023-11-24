0fe982c048ad7f1436e496178ea01d3040b1c735
estebancb03 <esteban.castaneda@ucr.ac.cr>
2023-11-23 19:13:45 -0600

    Estilos de mensaje

---
+++ src/Frontend/src/components/Message.jsx
@@ -0,0 +1,47 @@
+import { FaCircleUser, FaGuitar } from 'react-icons/fa6';
+
+import useAuth from '../hooks/useAuth';
+
+const Message = (props) => {
+  const { type, content } = props;
+  const { AuthContext: { userCredentials: { photoURL } } } = useAuth();
+
+  return (
+    <div className='w-full my-4 px-10 2xl:px-[27%]'>
+      {
+        type === 'user' ? (
+          <div>
+            {
+              photoURL ? (
+                <div className='flex'>
+                  <img className='w-[24px] h-[24px] rounded-full' src={photoURL} />
+                  <h1 className='mx-3 font-bold'>You</h1>
+                </div>
+              ) : (
+                <div className='flex'>
+                  <FaCircleUser size={'24px'} />
+                  <h1 className='mx-3 font-bold'>You</h1>
+                </div>
+              )
+            }
+            <div className='py-2 text-justify'>
+              <p className='ml-9'>{content}</p>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className='flex'>
+              <FaGuitar size={'24px'} />
+              <h1 className='mx-3 font-bold'>TylorSwiftGPT</h1>
+            </div>
+            <div className='py-2 text-justify'>
+              <p className='ml-9'>{content}</p>
+            </div>
+          </div>
+        )
+      }
+    </div>
+  )
+};
+
+export default Message;

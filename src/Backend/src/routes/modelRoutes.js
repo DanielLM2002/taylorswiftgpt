dba7c16b6cdb2ee31e62221935e594be5b94e243
Daniel Lizano <dlizano@outlook.com>
2023-12-03 01:31:12 -0600

    esteban maldito piedrero

--- src/Backend/src/routes/modelRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createSong } from '../models/model.js';
-
-const router = Router();
-
-router.post('/taylorswiftmodel', createSong);
-
-export default router;
